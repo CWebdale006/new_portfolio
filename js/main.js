b18027d27b50f1d8a8c4e3849ff2a7e6596af790
Connor Webdale <CWebdale006@west-mec.org>
2018-12-17 22:12:30 -0700

    Removed JavaScript on mobile view.  Added comments to my index and style sheet.  Added a favicon.  Added comments to my JavaScript file. Adds my resume to be downloaded.

--- js/main.js
+++ js/main.js
@@ -1,56 +1,45 @@
+// Created five functions that will show a paragraph when a div is hovered, when hide that paragraph when the div is no longer hovered 
+
+
 function showDesc1() {
-    document.getElementById('h1').style.opacity = '0';
     document.getElementById('p1').style.opacity = '1';
 }
-
 function hideDesc1() {
-    document.getElementById('h1').style.opacity = '1';
     document.getElementById('p1').style.opacity = '0';
 }
 
 
+
 function showDesc2() {
-    document.getElementById('h2').style.opacity = '0';
     document.getElementById('p2').style.opacity = '1';
 }
-
 function hideDesc2() {
-    document.getElementById('h2').style.opacity = '1';
     document.getElementById('p2').style.opacity = '0';
 }
 
 
 
 function showDesc3() {
-    document.getElementById('h3').style.opacity = '0';
     document.getElementById('p3').style.opacity = '1';
 }
-
 function hideDesc3() {
-    document.getElementById('h3').style.opacity = '1';
     document.getElementById('p3').style.opacity = '0';
 }
 
 
 
 function showDesc4() {
-    document.getElementById('h4').style.opacity = '0';
     document.getElementById('p4').style.opacity = '1';
 }
-
 function hideDesc4() {
-    document.getElementById('h4').style.opacity = '1';
     document.getElementById('p4').style.opacity = '0';
 }
 
 
 
 function showDesc5() {
-    document.getElementById('h5').style.opacity = '0';
     document.getElementById('p5').style.opacity = '1';
 }
-
 function hideDesc5() {
-    document.getElementById('h5').style.opacity = '1';
     document.getElementById('p5').style.opacity = '0';
 }
